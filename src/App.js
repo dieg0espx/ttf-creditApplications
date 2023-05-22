236618e22f7b984493af0f0bbb1747514c006857
diegoTrueZenit <diego@truezenithcreative.com>
2023-05-22 15:29:14 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, useActionData } from "react-router-dom";
+
+import CreditApplications from "./pages/CreditApplications";
+import Sheet from "./pages/Sheet";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div className="App">
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<CreditApplications />} />
+            </Routes>
+            <Routes>
+              <Route path="/sheet" element={<Sheet />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
